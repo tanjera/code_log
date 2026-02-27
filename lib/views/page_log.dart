163624c7fe52c6138f764c374082a702d71fad00
Ibi <ibi@altair.local>
2026-02-26 20:32:41 -0500

    0.7.9+34: fixed share_plus .pdf sharing on iOS

--- lib/views/page_log.dart
+++ lib/views/page_log.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
@@ -19,25 +21,38 @@ class PageLog extends StatefulWidget {
 }
 
 class PageLogState extends State<PageLog> {
-
-  Future<void> _sharePDF () async {
+  Future<void> _sharePDF() async {
     final file = await widget.log.pdf(widget.settings.pdfPageSize);
 
-    final String desc = "${DateFormat.yMMMMd().format(widget.log.created ?? DateTime.now())}, ${DateFormat.Hm().format(widget.log.created ?? DateTime.now())}"
+    final String desc =
+        "${DateFormat.yMMMMd().format(widget.log.created ?? DateTime.now())}, ${DateFormat.Hm().format(widget.log.created ?? DateTime.now())}"
         "${widget.log.identifier == null ? "" : ": ${widget.log.identifier}"}";
 
-    XFile xf = XFile(file.path,
-      mimeType: "application/pdf");
+    XFile xf = XFile(file.path, mimeType: "application/pdf");
 
     try {
-      await SharePlus.instance.share(
-        ShareParams(
-          files: [xf],
-          text: desc,
-          subject: desc,
-          downloadFallbackEnabled: true,
-        ),
-      );
+      if (Platform.isIOS && context.mounted) {
+        final box = context.findRenderObject() as RenderBox?;
+
+        await SharePlus.instance.share(
+          ShareParams(
+            files: [xf],
+            text: desc,
+            subject: desc,
+            downloadFallbackEnabled: true,
+            sharePositionOrigin: box!.localToGlobal(Offset.zero) & box.size,
+          ),
+        );
+      } else if (!Platform.isIOS) {
+        await SharePlus.instance.share(
+          ShareParams(
+            files: [xf],
+            text: desc,
+            subject: desc,
+            downloadFallbackEnabled: true,
+          ),
+        );
+      }
     } catch (e) {
       // ...
     }
@@ -46,95 +61,111 @@ class PageLogState extends State<PageLog> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-          title: Text("Log Viewer"),
-          actions: <Widget> [
-            IconButton(
-                icon: Icon(Icons.share_outlined),
-                tooltip: 'Share Log',
-                onPressed: _sharePDF
-            )
-          ]
-        ),
-        body: SafeArea(
-            child: Column (
-            mainAxisAlignment: .start,
-            crossAxisAlignment: .stretch,
-            children: [
-              Table(
-                defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-                columnWidths: const <int, TableColumnWidth>{
-                  0: FlexColumnWidth(1),
-                  1: FlexColumnWidth(3),
-                },
-                children: [
-                  TableRow(
-                    children: [
-                      Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text("Date & Time:")
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text("Log Viewer"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share_outlined),
+            tooltip: 'Share Log',
+            onPressed: _sharePDF,
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: .start,
+          crossAxisAlignment: .stretch,
+          children: [
+            Table(
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              columnWidths: const <int, TableColumnWidth>{
+                0: FlexColumnWidth(1),
+                1: FlexColumnWidth(3),
+              },
+              children: [
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text("Date & Time:"),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "${DateFormat.yMMMMd().format(widget.log.created ?? DateTime.now())}, ${DateFormat.Hm().format(widget.log.created ?? DateTime.now())}",
                       ),
-                      Padding(
-                        padding: EdgeInsets.all(10),
-                        child: Text("${DateFormat.yMMMMd().format(widget.log.created ?? DateTime.now())}, ${DateFormat.Hm().format(widget.log.created ?? DateTime.now())}" ),
-                      )
-                    ]
-                  ),
-                  TableRow(
-                      children: [
-                        Padding(
-                            padding: EdgeInsets.all(10),
-                            child: Text("Identifier:")
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: Text(widget.log.identifier ?? "--"),
-                        )
-                      ]
-                  )
-                ]
-              ),
+                    ),
+                  ],
+                ),
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text("Identifier:"),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.all(10),
+                      child: Text(widget.log.identifier ?? "--"),
+                    ),
+                  ],
+                ),
+              ],
+            ),
 
-              Expanded( child:ListView(
-                  scrollDirection: .vertical,
-                  children: widget.log.entries.map((item) =>
-                      Slidable(
-                          startActionPane: ActionPane(
-                            motion: const ScrollMotion(),
-                            extentRatio: .1,
-                            children: [
-                              SlidableAction(
-                                onPressed: (c) => setState(() {
-                                  item.redacted = !item.redacted;
-                                  widget.log.save();
-                                }),
-                                backgroundColor: item.redacted ? Colors.green : Colors.red,
-                                foregroundColor: Colors.white,
-                                icon: Icons.delete_outlined,
-                              ),
-                            ],
-                          ),
-
-                          child: Row(children:[ Padding(
-                            padding: widget.settings.eventLogCompact
-                                ? .symmetric(horizontal: 10, vertical: 5)
-                                : .symmetric(horizontal: 10, vertical: 10),
-                            child: Text("${item['occurred']}:\t${item['description']}",
-                              style: TextStyle(
-                                  fontSize: 14,
-                                  color: item.redacted ? Colors.grey : Colors.black,
-                                  decoration: item.redacted ? .lineThrough : null ),
+            Expanded(
+              child: ListView(
+                scrollDirection: .vertical,
+                children: widget.log.entries
+                    .map(
+                      (item) => Slidable(
+                        startActionPane: ActionPane(
+                          motion: const ScrollMotion(),
+                          extentRatio: .1,
+                          children: [
+                            SlidableAction(
+                              onPressed: (c) => setState(() {
+                                item.redacted = !item.redacted;
+                                widget.log.save();
+                              }),
+                              backgroundColor: item.redacted
+                                  ? Colors.green
+                                  : Colors.red,
+                              foregroundColor: Colors.white,
+                              icon: Icons.delete_outlined,
                             ),
-                          )]
-                          )
-                      )
-                  ).toList()
-              )
-              )
-            ],
-          )
-        )
+                          ],
+                        ),
+
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: widget.settings.eventLogCompact
+                                  ? .symmetric(horizontal: 10, vertical: 5)
+                                  : .symmetric(horizontal: 10, vertical: 10),
+                              child: Text(
+                                "${item['occurred']}:\t${item['description']}",
+                                style: TextStyle(
+                                  fontSize: 14,
+                                  color: item.redacted
+                                      ? Colors.grey
+                                      : Colors.black,
+                                  decoration: item.redacted
+                                      ? .lineThrough
+                                      : null,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    )
+                    .toList(),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
